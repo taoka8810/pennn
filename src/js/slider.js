14844fb1b688e72b3d482db4e8599891823b20f3
田岡勇人 <taoka.trombone@gmail.com>
2023-01-10 09:06:54 +0900

    レスポンシブ

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,8 +1,18 @@
 const swiper = new Swiper(".article-swiper", {
-  slidesPerView: 4,
-  spaceBetween: 50,
+  spaceBetween: 10,
+  slidesPerView: 2,
   navigation: {
     nextEl: ".article-swiper-button-next",
     prevEl: ".article-swiper-button-prev",
+  },
+  breakpoints: {
+    1050: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    560: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
   },
 });
