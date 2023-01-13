af3657d11b3cdff7528a8377f9c9932796083562
田岡勇人 <taoka.trombone@gmail.com>
2023-01-13 13:39:14 +0900

    アーカイブページの設定

--- src/php/functions.php
+++ src/php/functions.php
@@ -5,3 +5,14 @@ add_theme_support('post-thumbnails');
 
 // 管理画面へのバーを無効化する
 show_admin_bar(false);
+
+// 投稿のアーカイブページ
+add_filter('register_post_type_args', 'post_has_archive', 10, 2);
+function post_has_archive($args, $post_type)
+{
+    if ('post' == $post_type) {
+        $args['rewrite'] = true;
+        $args['has_archive'] = 'blog';
+    }
+    return $args;
+}
