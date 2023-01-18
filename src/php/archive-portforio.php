2fa9204f68d9c82c4a558843df15574121f6cc43
田岡勇人 <taoka.trombone@gmail.com>
2023-01-18 21:47:42 +0900

    sdf

--- src/php/archive-portforio.php
+++ src/php/archive-portforio.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
-<main>
-  ポートフォリオページです
+<main class="p-portforio">
+  <div class="p-portforio__inner">
+    <h1 class="p-portforio__title">Portforio</h1>
+    <p class="p-portforio__text">まだ掲載できるものがないんです(; ;)</p>
+  </div>
 </main>
 <?php get_footer(); ?>
--- src/php/page-about.php
+++ src/php/page-about.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main>
-  <h1>自己紹介</h1>
+  <h1>About Me</h1>
 </main>
 
 <?php get_footer(); ?>
