2a50cb5b800eb03f26cea185d769364e690e694e
田岡勇人 <taoka.trombone@gmail.com>
2023-02-02 22:53:48 +0900

    404, 500追加

---
+++ src/php/404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.min.css">
+  <title>ページが見つかりませんでした | Pennn</title>
+</head>
+<body>
+  
+</body>
+</html><main class="p-404">
+  <h1 class="p-404__title" style="color: white; font-size: 150px; font-weight: bold;">404</h1>
+</main>
